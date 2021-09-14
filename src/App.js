81c68cc4375c4a540250d15a39bfcc3e5593fe08
Dominic Law <dominic.law79@gmail.com>
2021-09-14 23:58:56 +1000

    Add navigation and header animation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// Component imports
+import HeaderAnimation from "./components/HeaderAnimation";
+import Nav from "./components/Nav";
+import logo from "./logo.svg";
+import bg from "./images/header1.jpg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="w-full h-screen bg-no-repeat bg-cover -mt-2" style={{backgroundImage: `url(${bg})`}}>
+        <div class="w-full h-screen bg-opacity-75 bg-gray-900">
+          <Nav />
+          <div class="flex items-center justify-center h-full w-full">
+            <div class="text-center">
+              <HeaderAnimation />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
