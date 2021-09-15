f389266dcc45af2c71ec134d73de38be89a5769d
Dominic Law <dominic.law79@gmail.com>
2021-09-16 01:49:07 +1000

    Add about page

---
+++ src/pages/About.js
@@ -0,0 +1,54 @@
+function About() {
+    return (
+        <section>
+            <h1 className="text-2xl font-bold py-10">Who am I?</h1>
+            <p>
+            Hi! My name is Dominic Law. I'm currently in my final year at Macquarie
+            <br />
+            University studying a Bachelor of Science with a major in Computing and
+            <br />
+            a Bachelor of Commerce with a major in Finance. Currently looking for
+            <br />
+            opportunities in the Software Development industry whether that is entry
+            <br />
+            level, internships or graduate roles.
+            <br />
+            <br />
+            Back in 2012, that was when my journey began, tinkering with websites
+            <br />
+            and getting my hands dirty with HTML and CSS. Almost a decade later,
+            <br />
+            I've expanded my skillset working with various languages for various
+            <br />
+            specialities, including:
+            <br />
+            <br />
+            &#8226; Java
+            <br />
+            &#8226; Python
+            <br />
+            &#8226; JavaScript
+            <br />
+            &#8226; HTML
+            <br />
+            &#8226; CSS
+            <br />
+            &#8226; SQL
+            <br />
+            <br />
+            These are also the frameworks I have tinkered with: <br />
+            <br />
+            &#8226; React Native
+            <br />
+            &#8226; Expo
+            <br />
+            &#8226; Vue.js
+            <br />
+            &#8226; Bootstrap
+            <br />
+            </p>
+        </section>
+    )
+}
+
+export default About;
