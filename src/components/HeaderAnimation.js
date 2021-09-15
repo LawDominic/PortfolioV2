3e900bc6c33c498bb7f207358758b73c14747a16
Dominic Law <dominic.law79@gmail.com>
2021-09-15 18:37:21 +1000

    Add HeaderAnimation

---
+++ src/components/HeaderAnimation.js
@@ -0,0 +1,24 @@
+import HeaderAnimationCSS from "./HeaderAnimation.css";
+
+function HeaderAnimation() {
+    return (
+        <div id="header-content" class="mt-96">
+        <p>
+            My name is <span class="highlight">Dominic Law</span>.<br />
+        </p>
+        <p>
+            I am currently in my <span class="highlight">final year</span> at
+            Macquarie University
+            <br />
+        </p>
+        <p>
+            actively pursuing a career in the{" "}
+            <span class="highlight">Software Development</span>
+            <br />
+        </p>
+        <p>landscape and aiming to perfect my craft.</p>
+        </div>
+    );
+}
+
+export default HeaderAnimation;
