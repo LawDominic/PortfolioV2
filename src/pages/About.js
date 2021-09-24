f72fbc2978975658917826e88193847023295c82
Dominic Law <dominic.law79@gmail.com>
2021-09-25 03:28:40 +1000

    Restructre about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,52 +1,33 @@
 function About() {
     return (
-        <section>
+        <section class="container mx-auto text-align-center sm:w-full md:w-1/3 lg:w-1/3">
             <h1 className="text-2xl font-bold py-10">Who am I?</h1>
-            <p>
-            Hi! My name is Dominic Law. I'm currently in my final year at Macquarie
-            <br />
-            University studying a Bachelor of Science with a major in Computing and
-            <br />
-            a Bachelor of Commerce with a major in Finance. Currently looking for
-            <br />
-            opportunities in the Software Development industry whether that is entry
-            <br />
-            level, internships or graduate roles.
-            <br />
-            <br />
-            Back in 2012, that was when my journey began, tinkering with websites
-            <br />
-            and getting my hands dirty with HTML and CSS. Almost a decade later,
-            <br />
-            I've expanded my skillset working with various languages for various
-            <br />
-            specialities, including:
-            <br />
-            <br />
-            &#8226; Java
-            <br />
-            &#8226; Python
-            <br />
-            &#8226; JavaScript
-            <br />
-            &#8226; HTML
-            <br />
-            &#8226; CSS
-            <br />
-            &#8226; SQL
-            <br />
-            <br />
-            These are also the frameworks I have tinkered with: <br />
-            <br />
-            &#8226; React Native
-            <br />
-            &#8226; Expo
-            <br />
-            &#8226; Vue.js
-            <br />
-            &#8226; Bootstrap
-            <br />
-            </p>
+            <div class="">
+                Hi! My name is Dominic Law. I'm currently in my final year at Macquarie University studying a Bachelor of Science with a major in Computing and a Bachelor of Commerce with a major in Finance. Currently looking for opportunities in the Software Development industry whether that is entry        level, internships or graduate roles. Back in 2012, that was when my journey began, tinkering with websites and getting my hands dirty with HTML and CSS. Almost a decade later, I've expanded my skillset working with various languages for various specialties, including:
+                <br />
+                <br />
+                <ul>
+                    <li>&#8226; Java</li>
+                    <li>&#8226; Python</li>
+                    <li>&#8226; ReactJS</li>
+                    <li>&#8226; JavaScript</li>
+                    <li>&#8226; HTML</li>
+                    <li>&#8226; CSS</li>
+                    <li>&#8226;SQL</li>
+                </ul>
+                <br />
+                <br />
+                These are also the frameworks I have tinkered with: <br />
+                <br/>
+                <ul>
+                    <li> &#8226; React Native</li>
+                    <li> &#8226; Expo</li>
+                    <li> &#8226; ReactJS</li>
+                    <li> &#8226; Vue.js</li>
+                    <li> &#8226; Boostrap</li>
+                    <li> &#8226; Tailwind CSS</li>
+                </ul>
+            </div>
         </section>
     )
 }
