b459662f182fbecb4d8a5b92ac04877b4726944f
Dominic Law <dominic.law79@gmail.com>
2021-09-25 03:25:09 +1000

    Add up botton and component divs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 // Component imports
+import React, {useState, useEffect} from 'react';
 import HeaderAnimation from "./components/HeaderAnimation";
 import Nav from "./components/Nav";
 import bg from "./images/header.jpg";
 import "./App.css";
+import FeatherIcon from 'feather-icons-react';
 
 // Page imports
 import About from './pages/About';
@@ -11,22 +13,39 @@ import Footer from './pages/Footer';
 import Projects from './pages/Projects';
 
 function App() {
+
+  const upTop = (event) => {
+    event.preventDefault();
+    window.scroll({top: 0, behavior: 'smooth'});
+  }
+
   return (
     <div className="App">
       <div class="w-full h-screen bg-no-repeat bg-cover -mt-2" style={{backgroundImage: `url(${bg})`}}>
         <div class="w-full h-screen bg-opacity-75 bg-gray-900">
           <Nav />
-          <div class="flex items-center justify-center h-full w-full mt-96 pl-36">
+          <div class="flex h-screen w-full lg:mt-96">
             <HeaderAnimation />
           </div>
         </div>
       </div>
       <section className="bg-gray-100">
-        <About />
-        <Projects />
-        <Contact />
-        <Footer />
+        <div id="about">
+          <About />
+        </div>
+        <div id="projects">
+          <Projects />
+        </div>
+        <div id="contact">
+          <Contact />
+        </div>
+        <div id="footer">
+          <Footer />
+        </div>
       </section>
+      <div class="fixed z-30 bottom-0 right-0 mr-8 mb-8">
+        <FeatherIcon icon="chevron-up" class="text-green-400 bg-gray-200 rounded-lg h-8 w-8" onClick={upTop}/>
+      </div>
     </div>
   );
 }
